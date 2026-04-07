d40b4be02e68786c9af20a8ae35ef4723466ba19
shivam <shivamkole39@gmail.com>
2026-04-07 17:22:06 +0530

    Fix __str__method in models.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")
-
-    )
+    'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"))
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
